042852a708caafb7bc9bc5162563da0e5e5f0fd2
MoemenMounir <moemena805@gmail.com>
2025-12-23 23:15:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,16 +24,12 @@ function displayArticles(list) {
     const card = document.createElement("div");
     card.className = "article-card";
 
-    const imgSrc = Array.isArray(article.image)
-  ? article.image[0]
-  : article.image;
-
-card.innerHTML = `
-  <h2>${article.title}</h2>
-  <img src="${imgSrc}" width="35%">
-  <p><strong>الكاتب:</strong> ${article.author} | <strong>التاريخ:</strong> ${article.date}</p>
-  <a href="article.html?id=${article.id}" id="aa">اقرأ المزيد</a>
-`;
+    card.innerHTML = `
+      <h2>${article.title}</h2>
+      <img src="${article.image}" width="35%">
+      <p><strong>الكاتب:</strong> ${article.author} | <strong>التاريخ:</strong> ${article.date}</p>
+      <a href="article.html?id=${article.id}" id="aa">اقرأ المزيد</a>
+    `;
 
     container.appendChild(card);
   });
